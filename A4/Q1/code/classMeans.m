ce9b1dcb9d98d674c8bd1ffa36e8c72649e42a01
Niranjan Thakurdesai <niranjantdesai@gmail.com>
2016-04-05 01:04:01 +0530

    Updated classMeans (again)

--- A4/Q1/code/classMeans.m
+++ A4/Q1/code/classMeans.m
@@ -1,7 +1,6 @@
     function [ c ] = classMeans( u,y,w,b,mask,q,K )
 %classMeans Finds the optimal value of class means within every iteration
 
-imgSize = size(y);
 c = zeros(K,1);
 y = y.*mask;
 innerSumNum = conv2(b,w);
