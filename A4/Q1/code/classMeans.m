87ef7b58b68270caa789724112ada52586554a5b
Niranjan Thakurdesai <niranjantdesai@gmail.com>
2016-04-05 00:59:11 +0530

    Updated classMeans

--- A4/Q1/code/classMeans.m
+++ A4/Q1/code/classMeans.m
@@ -1,21 +1,15 @@
-function [ c ] = classMeans( u,y,w,b,mask,q,K )
+    function [ c ] = classMeans( u,y,w,b,mask,q,K )
 %classMeans Finds the optimal value of class means within every iteration
 
 imgSize = size(y);
 c = zeros(K,1);
+y = y.*mask;
 innerSumNum = conv2(b,w);
 innerSumDenom = conv2(b.^2,w);
+
 for k = 1:K
-    num=0;
-    denom=0;
-    for i = 1:imgSize(1)
-        for j = 1:imgSize(2)
-            if (mask(i,j) > 0)
-                num = num + innerSumNum(i,j)*(u(i,j,k)^q)*y(i,j);
-                denom = denom + innerSumDenom(i,j)*(u(i,j,k)^q);
-            end
-        end
-    end
+    num = sum(sum((u(:,:,k).^q).*y.*innerSumNum));
+    denom = sum(sum((u(:,:,k).^q).*innerSumDenom));
     c(k) = num/denom;
 end
 
