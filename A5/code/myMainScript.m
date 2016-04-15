96299abbd16ac4d76091c337209d9da0277c69e3
Ayush Baid <ayushrakeshbaid@gmail.com>
2016-04-16 00:23:44 +0530

    A5: variance calc (incorrect)

--- A5/code/myMainScript.m
+++ A5/code/myMainScript.m
@@ -1,4 +1,7 @@
 % Shape analysis
+clc
+clear all;
+close;
 
 %% Loading data
 load('../data/assignmentShapeAnalysis.mat');
@@ -39,21 +42,26 @@ end
 hold off
 
 %% Mean shape calculation
-% using the first point set as a point
-mean = preshapePointSets(:,:,1);
+% using a random point set as the mean initialization
+initIndex = unidrnd(numSets,1);
+mean = preshapePointSets(:,:,initIndex);
+newMean = mean;
 
 % params for iteration 
-diffThreshold = 1e-4;
+diffThreshold = 1e-6;
 maxIters = 25;
 
 iter = 1;
 diff = 1e3;
 while(diff>diffThreshold && iter<maxIters)
+    mean = newMean;
     for i=1:numSets
-        R = align(mean,preshapePointSets(:,:,i));
+        R = procrustes(mean,preshapePointSets(:,:,i));
         preshapePointSets(:,:,i) = R*preshapePointSets(:,:,i);
     end
-    newMean = sum(preShapePointSets,3)/numSets;
+    
+    % Finding optimal mean shape within each iteration
+    newMean = sum(preshapePointSets,3)/numSets;
     
     % normalizing mean to bring it into preshape space; centroid already at
     % origin
@@ -63,11 +71,52 @@ while(diff>diffThreshold && iter<maxIters)
     
     % calculate the difference between the means
     diff = sqrt(sumsqr(mean-newMean));
+    % disp(diff);
     iter = iter+1;
 end
 
 figure()
 scatter(mean(1,:),mean(2,:));
-title('Mean shape');
+title('Final Mean shape');
+
+
+%% Plotting aligned point sets
+
+color_list=jet(numSets);
+
+figure();
+hold on;
+% scatter(mean(1,:),mean(2,:),16,'MarkerFaceColor',[0 0 0]);
+plot(mean(1,:),mean(2,:),'-x','LineWidth',4,'Marker','*');
+for i=1:numSets
+   scatter(preshapePointSets(1,:,i),preshapePointSets(2,:,i),...
+       4,'MarkerFaceColor',color_list(i,:)); 
+end
+title('Aligned Point Sets');
+hold off
+
+%% Computing principal modes of variation
+vectorizedPoints = zeros(size(pointSets,1)*size(pointSets,2),size(pointSets,3));
+
+cov = 0;
+% mean subtracted vectorized points
+for i=1:numSets
+    vectorizedPoints(:,i) = [preshapePointSets(1,:,i) preshapePointSets(2,:,i)]' - vectorizedMean;
+    cov = cov + vectorizedPoints(:,i)*vectorizedPoints(:,i)';
+end
+cov = cov/numSets;
+
+
+% Eigenvalue decomposition
+[V,D] = eig(cov);
+
+eigenvals = diag(D);
+
+figure()
+scatter(1:length(eigenvals),eigenvals);
+title('Variances along each principal mode');
+
+
+
 
 
